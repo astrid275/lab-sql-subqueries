79cf6d03b657efd5a3dc21aa48a778e49eaa50de
astrid275 <astrid275@gmail.com>
2025-07-24 14:32:01 +0200

    Solved Lab
    Questions 1-3
    without Bonus questions 4-8

---
+++ Lab SQL Subqueries.sql
@@ -0,0 +1,25 @@
+# Challenge
+# Write SQL queries to perform the following tasks using the Sakila database:
+
+# 1: Determine the number of copies of the film "Hunchback Impossible" that exist in the inventory system.
+SELECT COUNT(*) AS num_copies
+FROM inventory
+WHERE film_id = (SELECT film_id
+				FROM film
+				WHERE title = "Hunchback Impossible");
+
+# 2: List all films whose length is longer than the average length of all the films in the Sakila database.
+SELECT title
+FROM film
+WHERE length > (SELECT round(avg(length))
+	FROM film);
+
+# 3: Use a subquery to display all actors who appear in the film "Alone Trip".
+
+SELECT actor_id
+FROM film_actor
+WHERE film_id = (SELECT film_id
+				FROM film
+				WHERE title = "Alone Trip");
+
+
